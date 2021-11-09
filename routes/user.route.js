62de5c7e62f090aae68912ba601e6e2317d8fde8
huydevct <huyvipct1@gmail.com>
2021-11-10 00:40:02 +0700

    logout user

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,7 +4,13 @@ const createError = require("http-errors");
 
 const User = require("../models/user.model");
 const { userValidate } = require("../config/validation");
-const {signAccessToken, verifyAccessToken, signRefreshToken, verifyRefreshToken} = require('../config/jwt_service')
+const {
+  signAccessToken,
+  verifyAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require("../config/jwt_service");
+const client  = require("../config/connection_redis");
 
 userRouter.post("/register", async (req, res, next) => {
   try {
@@ -48,73 +54,91 @@ userRouter.post("/register", async (req, res, next) => {
   }
 });
 
-userRouter.post('/login', async (req,res,next) => {
+userRouter.post("/login", async (req, res, next) => {
   try {
-    const {error} = userValidate(req.body);
-    if(error){
+    const { error } = userValidate(req.body);
+    if (error) {
       throw createError(error.details[0].message);
     }
 
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
     const user = await User.findOne({
-      username
+      username,
     });
-    if(!user){
-      throw createError.NotFound('User not registered')
+    if (!user) {
+      throw createError.NotFound("User not registered");
     }
 
     const isValid = await user.isCheckPassword(password);
-    if(!isValid){
+    if (!isValid) {
       throw createError.Unauthorized();
     }
     const accessToken = await signAccessToken(user._id);
     const refreshToken = await signRefreshToken(user._id);
     res.json({
       accessToken,
-      refreshToken
-    })
+      refreshToken,
+    });
   } catch (error) {
     next(error);
   }
 });
 
-userRouter.get('/getlists', verifyAccessToken,  (req,res,next) => {
+userRouter.get("/getlists", verifyAccessToken, (req, res, next) => {
   console.log(req.headers);
   const listUsers = [
     {
-      username: 'abc@gmail.com',
+      username: "abc@gmail.com",
     },
     {
-      username: 'def@gmail.com',
+      username: "def@gmail.com",
     },
   ];
-  
+
   res.json({
-    listUsers
-  })
+    listUsers,
+  });
 });
 
-userRouter.post('/refresh-token', async (req,res,next) => {
+userRouter.post("/refresh-token", async (req, res, next) => {
   try {
     console.log(req.body);
-    const {refreshToken} = req.body;
-    if(!refreshToken) throw createError.BadRequest();
+    const { refreshToken } = req.body;
+    if (!refreshToken) throw createError.BadRequest();
 
-    const {userId} = await verifyRefreshToken(refreshToken);
+    const { userId } = await verifyRefreshToken(refreshToken);
     const accessToken = await signAccessToken(userId);
     const refToken = await signRefreshToken(userId);
 
     res.json({
       accessToken,
       refreshToken: refToken,
-    })
-
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
+userRouter.delete("/logout", async (req, res, next) => {
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      throw createError.BadRequest();
+    }
 
+    const { userId } = await verifyRefreshToken(refreshToken);
+    client.del(userId.toString(), (err, reply) => {
+      if (err) {
+        throw createError.InternalServerError();
+      }
+      res.json({
+        message: "Logout",
+      });
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = userRouter;
