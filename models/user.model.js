80c26fe9513bd8643ac60a9ac918793553e0d08a
huydevct <huyvipct1@gmail.com>
2021-11-09 17:44:19 +0700

    login user with bcrypt

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     username: {
@@ -13,5 +14,25 @@ const UserSchema = new Schema({
         required: true,
     },
 })
+
+UserSchema.pre('save', async function(next){
+    try {
+        console.log(`Called before save:`, this.username, this.password);
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+UserSchema.methods.isCheckPassword = async function(password){
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        
+    }
+}
 
 module.exports = mongoose.model("user", UserSchema);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,45 +1,79 @@
-const express = require('express')
+const express = require("express");
 const userRouter = express.Router();
-const createError = require('http-errors');
+const createError = require("http-errors");
 
-const User = require('../models/user.model');
-const {userValidate} = require('../config/validation');
+const User = require("../models/user.model");
+const { userValidate } = require("../config/validation");
 
-userRouter.post('/register', async (req,res,next) => {
-    try {
-        const { email, password } = req.body;
-        const {error} = userValidate(req.body);
-        console.log(`error validate: ${error}`);
-        if(error){
-            throw createError(error.details[0].message)
-        }
-
-        // if(!email || !password){
-        //     throw createError.BadRequest();
-        // }
-
-
-
-        const isExist = await User.findOne({
-            username: email
-        })
-
-        if(isExist){
-            throw createError.Conflict(`${email} is ready been register`);
-        }
-
-        const isCreate = await User.create({
-            username: email,
-            password
-        })
-
-        return res.json({
-            status: 'OK',
-            elements: isCreate
-        })
-    } catch (error) {
-        next(error)
+userRouter.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const { error } = userValidate(req.body);
+    console.log(`error validate: ${error}`);
+    if (error) {
+      throw createError(error.details[0].message);
     }
-})
 
-module.exports = userRouter
+    // if(!username || !password){
+    //     throw createError.BadRequest();
+    // }
+
+    const isExist = await User.findOne({
+      username,
+    });
+
+    if (isExist) {
+      throw createError.Conflict(`${username} is ready been register`);
+    }
+
+    // const isCreate = await User.create({
+    //     username: username,
+    //     password
+    // })
+
+    const user = new User({
+      username,
+      password,
+    });
+
+    const savedUser = await user.save();
+
+    return res.json({
+      status: "OK",
+      elements: savedUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.post('/login', async (req,res,next) => {
+  try {
+    const {error} = userValidate(req.body);
+    if(error){
+      throw createError(error.details[0].message);
+    }
+
+    const {username, password} = req.body;
+
+    const user = await User.findOne({
+      username
+    });
+    if(!user){
+      throw createError.NotFound('User not registered')
+    }
+
+    const isValid = await user.isCheckPassword(password);
+    if(!isValid){
+      throw createError.Unauthorized();
+    }
+
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
+module.exports = userRouter;
